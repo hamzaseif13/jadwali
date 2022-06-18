6a6e6a86af41ebba729d7a22cbdc6e867d0ada7d
unknown <hamzaseif1333@gmail.com>
2022-06-18 17:45:11 +0300

    f

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,9 @@ const PORT=process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static("client/build"))
+}
 app.use(searchController)
 app.use(generateController)
 
