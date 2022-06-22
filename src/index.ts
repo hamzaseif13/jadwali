c431790e55ad222f705135b6b8e1b67fd63007c9
unknown <hamzaseif1333@gmail.com>
2022-06-22 19:26:36 +0300

    a

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ AppDataSource.initialize()
 
 const app: Express = express();
 const PORT=process.env.PORT || 5050;
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static("client/build"));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
