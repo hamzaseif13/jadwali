8bf32a71075d104f5d3d4e0a32b09d5e46f9ebd5
unknown <hamzaseif1333@gmail.com>
2022-09-18 20:43:43 +0300

    fixed all departments bug

--- src/service/searchService.ts
+++ src/service/searchService.ts
@@ -7,7 +7,7 @@ const search=async(req: Request, res: Response)=>{
         if (department==='All'){
             department=''
         }
-        const courses= await courseRepo.createQueryBuilder("course").select("course").where(`course.department="${department}" 
+        const courses= await courseRepo.createQueryBuilder("course").select("course").where(`course.department like "%${department}%" 
         and (course.name like '%${query}%' or course.symbol like '%${query}%' or course.line_number like '%${query}%'  )`).limit(10).getMany()
         
         res.json(courses)
