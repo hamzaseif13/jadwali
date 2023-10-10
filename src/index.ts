84719411df59148ff8c9fceed31c1472c1e82d13
hamza <hamzaseif1333@gmail.com>
2023-10-10 22:56:51 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,31 @@
 import { AppDataSource } from "./config/DataSource";
 // @ts-ignore
-import cors from 'cors'
+import cors from "cors";
 import express, { Express } from "express";
-import path from 'path'
-import {searchRouter} from './routers/searchRouter';
+import path from "path";
+import { searchRouter } from "./routers/searchRouter";
 import { generateRouter } from "./routers/generateRouter";
-import compression from 'compression'
+import compression from "compression";
 import { feedbackRouter } from "./routers/feedbackRouter";
 import { sectionsRouter } from "./routers/sectionsRouter";
 import { lastUpdatedRouter } from "./routers/lastUpdatedRouter";
 AppDataSource.initialize()
-  .then(() => {console.log("db connected")})
+  .then(() => {
+    console.log("db connected");
+  })
   .catch((err) => console.log(err));
 const app: Express = express();
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.use(compression())
+app.use(compression());
 app.use(express.json());
-app.use(cors())
-app.use(searchRouter)
-app.use(generateRouter)
-app.use(feedbackRouter)
-app.use(sectionsRouter)
-app.use(lastUpdatedRouter)
-
-
-app.get("/*",(req,res)=>{
-  res.sendFile(path.join(__dirname+'/../client/build/index.html'))
-})
-
-
+app.use(cors());
+app.use(searchRouter);
+app.use(generateRouter);
+app.use(feedbackRouter);
+app.use(sectionsRouter);
+app.use(lastUpdatedRouter);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
-
